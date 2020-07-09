2e9d5f4655482a21e77ac2cf502338bf08f9690b
jorgevillaverde-sitewhere <jorge.villaverde@sitewhere.com>
2020-07-09 12:05:45 -0300

    fix: add svc create/delete tests

--- internal/operations_test.go
+++ internal/operations_test.go
@@ -781,3 +781,126 @@ func TestDeletePersistentVolumeClaimIfExists(t *testing.T) {
 		}(single))
 	}
 }
+
+func TestCreateServiceIfNotExists(t *testing.T) {
+	t.Parallel()
+	data := []struct {
+		svc       v1.Service
+		namespace string
+		clientset kubernetes.Interface
+		err       error
+	}{
+		// Service exists, should return existing
+		{
+			svc: v1.Service{ObjectMeta: metav1.ObjectMeta{
+				Name:        "existing",
+				Namespace:   "ns",
+				Annotations: map[string]string{},
+			}},
+			namespace: "ns",
+			clientset: fake.NewSimpleClientset(&v1.Service{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:        "existing",
+					Namespace:   "ns",
+					Annotations: map[string]string{},
+				},
+			}),
+		},
+		// Service does not exist, should return created ns
+		{
+			svc: v1.Service{ObjectMeta: metav1.ObjectMeta{
+				Name:        "existing",
+				Namespace:   "ns",
+				Annotations: map[string]string{},
+			}},
+			namespace: "ns",
+			clientset: fake.NewSimpleClientset(),
+		},
+	}
+	for _, single := range data {
+		t.Run(single.svc.ObjectMeta.Name, func(single struct {
+			svc       v1.Service
+			namespace string
+			clientset kubernetes.Interface
+			err       error
+		}) func(t *testing.T) {
+			return func(t *testing.T) {
+				result, err := CreateServiceIfNotExists(&single.svc, single.clientset, single.namespace)
+
+				if err != nil {
+					if single.err == nil {
+						t.Fatalf(err.Error())
+					}
+					if !strings.EqualFold(single.err.Error(), err.Error()) {
+						t.Fatalf("expected err: %s got err: %s", single.err, err)
+					}
+				} else {
+					if result.ObjectMeta.Name != single.svc.ObjectMeta.Name {
+						t.Fatalf("expected %s service, got %s", single.svc.ObjectMeta.Name, result.ObjectMeta.Name)
+					}
+				}
+			}
+		}(single))
+	}
+}
+
+func TestDeleteServiceIfExists(t *testing.T) {
+
+	t.Parallel()
+	data := []struct {
+		svc       v1.Service
+		namespace string
+		clientset kubernetes.Interface
+		err       error
+	}{
+		// Service exists, should return existing
+		{
+			svc: v1.Service{ObjectMeta: metav1.ObjectMeta{
+				Name:        "existing",
+				Namespace:   "ns",
+				Annotations: map[string]string{},
+			}},
+			namespace: "ns",
+			clientset: fake.NewSimpleClientset(&v1.Service{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:        "existing",
+					Namespace:   "ns",
+					Annotations: map[string]string{},
+				},
+			}),
+		},
+		// Service does not exist, should return created ns
+		{
+			svc: v1.Service{ObjectMeta: metav1.ObjectMeta{
+				Name:        "non-existing",
+				Namespace:   "ns",
+				Annotations: map[string]string{},
+			}},
+			namespace: "ns",
+			clientset: fake.NewSimpleClientset(),
+			err:       fmt.Errorf("services \"non-existing\" not found"),
+		},
+	}
+
+	for _, single := range data {
+		t.Run(single.svc.ObjectMeta.Name, func(single struct {
+			svc       v1.Service
+			namespace string
+			clientset kubernetes.Interface
+			err       error
+		}) func(t *testing.T) {
+			return func(t *testing.T) {
+				err := DeleteServiceIfExists(&single.svc, single.clientset, single.namespace)
+
+				if err != nil {
+					if single.err == nil {
+						t.Fatalf(err.Error())
+					}
+					if !strings.EqualFold(single.err.Error(), err.Error()) {
+						t.Fatalf("expected err: %s got err: %s", single.err, err)
+					}
+				}
+			}
+		}(single))
+	}
+}
