841e4f2c2f6d360915df5a9031819065cc1f389a
jorgevillaverde-sitewhere <jorge.villaverde@sitewhere.com>
2020-07-11 21:15:10 -0300

    fix: add deployments create/delete test

--- internal/operations_test.go
+++ internal/operations_test.go
@@ -14,6 +14,7 @@ import (
 	"strings"
 	"testing"
 
+	appsv1 "k8s.io/api/apps/v1"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
@@ -891,6 +892,129 @@ func TestDeleteServiceIfExists(t *testing.T) {
 		}) func(t *testing.T) {
 			return func(t *testing.T) {
 				err := DeleteServiceIfExists(&single.svc, single.clientset, single.namespace)
+
+				if err != nil {
+					if single.err == nil {
+						t.Fatalf(err.Error())
+					}
+					if !strings.EqualFold(single.err.Error(), err.Error()) {
+						t.Fatalf("expected err: %s got err: %s", single.err, err)
+					}
+				}
+			}
+		}(single))
+	}
+}
+
+func TestCreateDeploymentIfNotExists(t *testing.T) {
+	t.Parallel()
+	data := []struct {
+		svc       appsv1.Deployment
+		namespace string
+		clientset kubernetes.Interface
+		err       error
+	}{
+		// Deployment exists, should return existing
+		{
+			svc: appsv1.Deployment{ObjectMeta: metav1.ObjectMeta{
+				Name:        "existing",
+				Namespace:   "ns",
+				Annotations: map[string]string{},
+			}},
+			namespace: "ns",
+			clientset: fake.NewSimpleClientset(&appsv1.Deployment{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:        "existing",
+					Namespace:   "ns",
+					Annotations: map[string]string{},
+				},
+			}),
+		},
+		// Deployment does not exist, should return created ns
+		{
+			svc: appsv1.Deployment{ObjectMeta: metav1.ObjectMeta{
+				Name:        "existing",
+				Namespace:   "ns",
+				Annotations: map[string]string{},
+			}},
+			namespace: "ns",
+			clientset: fake.NewSimpleClientset(),
+		},
+	}
+	for _, single := range data {
+		t.Run(single.svc.ObjectMeta.Name, func(single struct {
+			svc       appsv1.Deployment
+			namespace string
+			clientset kubernetes.Interface
+			err       error
+		}) func(t *testing.T) {
+			return func(t *testing.T) {
+				result, err := CreateDeploymentIfNotExists(&single.svc, single.clientset, single.namespace)
+
+				if err != nil {
+					if single.err == nil {
+						t.Fatalf(err.Error())
+					}
+					if !strings.EqualFold(single.err.Error(), err.Error()) {
+						t.Fatalf("expected err: %s got err: %s", single.err, err)
+					}
+				} else {
+					if result.ObjectMeta.Name != single.svc.ObjectMeta.Name {
+						t.Fatalf("expected %s service, got %s", single.svc.ObjectMeta.Name, result.ObjectMeta.Name)
+					}
+				}
+			}
+		}(single))
+	}
+}
+
+func TestDeleteDeploymentIfExists(t *testing.T) {
+
+	t.Parallel()
+	data := []struct {
+		depl      appsv1.Deployment
+		namespace string
+		clientset kubernetes.Interface
+		err       error
+	}{
+		// Deployment exists, should return existing
+		{
+			depl: appsv1.Deployment{ObjectMeta: metav1.ObjectMeta{
+				Name:        "existing",
+				Namespace:   "ns",
+				Annotations: map[string]string{},
+			}},
+			namespace: "ns",
+			clientset: fake.NewSimpleClientset(&appsv1.Deployment{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:        "existing",
+					Namespace:   "ns",
+					Annotations: map[string]string{},
+				},
+			}),
+		},
+		// Deployment does not exist, should return created ns
+		{
+			depl: appsv1.Deployment{ObjectMeta: metav1.ObjectMeta{
+				Name:        "non-existing",
+				Namespace:   "ns",
+				Annotations: map[string]string{},
+			}},
+			namespace: "ns",
+			clientset: fake.NewSimpleClientset(),
+			err:       fmt.Errorf("deployments.apps \"non-existing\" not found"),
+		},
+	}
+
+	for _, single := range data {
+		t.Run(single.depl.ObjectMeta.Name, func(single struct {
+			depl      appsv1.Deployment
+			namespace string
+			clientset kubernetes.Interface
+			err       error
+		}) func(t *testing.T) {
+			return func(t *testing.T) {
+				err := DeleteDeploymentIfExists(&single.depl, single.clientset, single.namespace)
 
 				if err != nil {
 					if single.err == nil {
