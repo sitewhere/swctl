096c618fde33bc541b3cbfbe9acc562dbf1de8c0
jorgevillaverde-sitewhere <jorge.villaverde@sitewhere.com>
2020-08-07 15:32:24 -0300

    fix: delete instance only delete microservices.

--- cmd/delete_instance.go
+++ cmd/delete_instance.go
@@ -17,10 +17,12 @@ import (
 
 	"github.com/sitewhere/swctl/internal"
 	"github.com/sitewhere/swctl/pkg/apis/v1/alpha3"
+
 	"github.com/spf13/cobra"
 	v1 "k8s.io/api/core/v1"
 	k8serror "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes"
 )
@@ -80,13 +82,19 @@ func deleteSiteWhereInstance(instance *alpha3.SiteWhereInstance) error {
 		return err
 	}
 
-	err = deleteSiteWhereResources(instance, client)
+	err = deleteSiteWhereMicroservicesResources(instance, client)
 	if err != nil {
 		fmt.Printf("Error deleting instance: %v\n", err)
 		return err
 	}
 
 	if purgeInstance {
+		err = deleteSiteWhereResources(instance, client)
+		if err != nil {
+			fmt.Printf("Error deleting instance: %v\n", err)
+			return err
+		}
+
 		err = deleteSiteWhereNamespace(instance, config)
 		if err != nil {
 			fmt.Printf("Error deleting namespace: %v\n", err)
@@ -97,6 +105,57 @@ func deleteSiteWhereInstance(instance *alpha3.SiteWhereInstance) error {
 	fmt.Printf("SiteWhere Instance '%s' deleted\n", instance.Name)
 
 	return err
+}
+
+func deleteSiteWhereMicroservicesResources(instance *alpha3.SiteWhereInstance, client dynamic.Interface) error {
+
+	res := client.Resource(sitewhereMicroserviceGVR).Namespace(instance.Namespace)
+
+	microservices, err := res.List(context.TODO(), metav1.ListOptions{})
+
+	// delete instance
+	if k8serror.IsNotFound(err) {
+		errorMessage := fmt.Sprintf("SiteWhere Microservices '%s' not found in namespace '%s'", instance.Name, instance.Namespace)
+		return errors.New(errorMessage)
+	}
+	if err != nil {
+		return err
+	}
+
+	for _, microservice := range microservices.Items {
+		metadata, exists, err := unstructured.NestedMap(microservice.Object, "metadata")
+
+		if err != nil {
+			fmt.Printf("Error reading metadata for %s: %v\n", instance.Name, err)
+			return nil
+		}
+		if !exists {
+			fmt.Printf("Metadata not found for for SiteWhere Instance: %s", instance.Name)
+		} else {
+			name, exists, err := unstructured.NestedString(metadata, "name")
+
+			if err != nil {
+				fmt.Printf("Error reading metadata for %s: %v\n", instance.Name, err)
+				return nil
+			}
+			if !exists {
+				fmt.Printf("Metadata not found for for SiteWhere Instance: %s", instance.Name)
+			} else {
+				err = res.Delete(context.TODO(), name, metav1.DeleteOptions{})
+
+				if k8serror.IsNotFound(err) {
+					errorMessage := fmt.Sprintf("SiteWhere Microservice '%s' not found in namespace '%s'", name, instance.Namespace)
+					return errors.New(errorMessage)
+				}
+				if err != nil {
+					return err
+				}
+			}
+		}
+	}
+
+	return nil
+	//	return res.Delete(context.TODO(), instance.Name, metav1.DeleteOptions{})
 }
 
 func deleteSiteWhereResources(instance *alpha3.SiteWhereInstance, client dynamic.Interface) error {
