ebb3ba06ccf64012bd096e0fe674dd0659b269a1
jorgevillaverde-sitewhere <jorge.villaverde@sitewhere.com>
2021-01-20 13:59:42 -0300

    fix: add test for CheckIfExistsNamespace

--- pkg/resources/namespaces_test.go
+++ pkg/resources/namespaces_test.go
@@ -169,3 +169,59 @@ func TestDeleteSiteWhereNamespaceIfExists(t *testing.T) {
 		}(single))
 	}
 }
+
+func TestCheckIfExistsNamespace(t *testing.T) {
+
+	t.Parallel()
+	data := []struct {
+		namespace string
+		clientset kubernetes.Interface
+		result    bool
+		err       error
+	}{
+		// Namespaces exists, should return existing
+		{
+			namespace: "existing",
+			clientset: fake.NewSimpleClientset(&v1.Namespace{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:        "existing",
+					Annotations: map[string]string{},
+				},
+			}),
+			result: true,
+			err:    nil,
+		},
+		// Namespaces does not exist, should return created ns
+		{
+			namespace: "non-existing",
+			clientset: fake.NewSimpleClientset(),
+			result:    false,
+			err:       nil,
+		},
+	}
+	for _, single := range data {
+		t.Run(single.namespace, func(single struct {
+			namespace string
+			clientset kubernetes.Interface
+			result    bool
+			err       error
+		}) func(t *testing.T) {
+			return func(t *testing.T) {
+				result, err := CheckIfExistsNamespace(single.namespace, single.clientset)
+
+				if err != nil {
+					if single.err == nil {
+						t.Fatalf(err.Error())
+					}
+					if !strings.EqualFold(single.err.Error(), err.Error()) {
+						t.Fatalf("expected err: %s got err: %s", single.err, err)
+					}
+				} else {
+					if result != single.result {
+						t.Fatalf("expected %t, got %t", single.result, result)
+					}
+				}
+			}
+		}(single))
+	}
+}
