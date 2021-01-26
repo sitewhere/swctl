68464100751ed8084b3175ddde7b28b6528c7094
jorgevillaverde-sitewhere <jorge.villaverde@sitewhere.com>
2021-01-26 16:31:25 -0300

    feat: update default tag to latest

--- pkg/action/create_instance.go
+++ pkg/action/create_instance.go
@@ -84,7 +84,7 @@ type instanceResourcesResult struct {
 }
 
 // SiteWhere Docker Image default tag name
-const dockerImageDefaultTag = "3.0.1"
+const dockerImageDefaultTag = "latest"
 
 // Default configuration Template
 const defaultConfigurationTemplate = "default"
